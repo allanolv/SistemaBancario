a9398f58d948b5a54ffe25e267aa47d19acb1a5b
GLEBSON GONZAGA <glebson.tomoru@gmail.com>
2012-10-28 20:11:48 -0200

    Glebson Gonzaga
    Alteracao da Classe Conta Normal e seu respectivo Teste

--- Fontes/Infnet.EngSoftSistBancario.Modelo/ContaCorrente/Normal.cs
+++ Fontes/Infnet.EngSoftSistBancario.Modelo/ContaCorrente/Normal.cs
@@ -2,16 +2,81 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Infnet.EngSoftSistBancario.Modelo;
 
-namespace Infnet.EngSoftSistBancario.Modelo
+namespace Infnet.EngSoftSistBancario.Modelo.ContaCorrente
 {
-    public class Normal : ContaCorrente  
+    public class Normal : ContaCorrente
     {
-        public int agencia { get; set; }
-        public int numeroDaConta { get; set; }
-        public decimal tarifaMensal { get; set; }
-        public int debitoBancario { get; set; }
-      
+        public Cliente nome;
+
+        public Cliente Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+        public int numeroDaconta;
+
+        public int NumeroDaconta
+        {
+            get { return numeroDaconta; }
+            set { numeroDaconta = value; }
+        }
+        public bool statusDaConta;
+
+        public bool StatusDaConta
+        {
+            get { return statusDaConta; }
+            set { statusDaConta = value; }
+        }
+        public decimal tarifaMensal;
+
+
+        public decimal Tarifa
+        {
+            get { return tarifaMensal; }
+            set { tarifaMensal = value; }
+        }
+        public Normal() { 
+        
+        }
+
+        public Normal(Cliente _nome, int _numeroDaconta, bool _statusDaConta, decimal _tarifaMensal)
+        {
+            this.Nome = _nome;
+            this.NumeroDaconta = _numeroDaconta;
+            this.StatusDaConta = _statusDaConta;
+            this.Tarifa = _tarifaMensal;
+        }
+
+
+        public override string DescricaoDaConta()
+        {
+         
+                return  "Informacao da Conta:" + this.Nome + this.NumeroDaconta + this.Tarifa;
+
+        }
+
+
+        public void setBloqueado(bool bloqueado)
+        {
+            this.StatusDaConta = bloqueado;
+        }
+
+        public bool getBloqueado()
+        {
+            return StatusDaConta;
+        }
+
+        public void Bloqueado()
+        {
+            setBloqueado(true);
+        }
+
+        public void Desbloqueado()
+        {
+            setBloqueado(false);
+        }
 
     }
 }
---
+++ Fontes/Infnet.EngSoftSistBancario.MsTestes/ContaNormalTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Infnet.EngSoftSistBancario.Modelo.ContaCorrente;
+using Infnet.EngSoftSistBancario.Modelo;
+
+namespace Infnet.EngSoftSistBancario.MsTestes
+{
+    [TestClass]
+    public class ContaNormalTest
+    {
+
+
+        [TestMethod()]
+        public void ClienteNormalContaTest()
+        {
+           
+            Normal target = new Normal(); 
+
+            Cliente esperado = new Cliente(); 
+            esperado.Nome = "Glebson";
+            Cliente retornado;
+            target.Nome = esperado;
+            retornado = target.Nome;
+            Assert.AreEqual(esperado, retornado);
+
+        }
+
+        [TestMethod()]
+        public void ContaNormalContaTest()
+        {
+            Normal target = new Normal();
+            int esperado = int.Parse("030");
+            int retornado;
+            target.NumeroDaconta = esperado;
+            retornado = target.NumeroDaconta;
+            Assert.AreEqual(esperado, retornado);
+        }
+
+        [TestMethod()]
+        public void TaxaNormalContaTest()
+        {
+            Normal target = new Normal();
+            decimal esperado = decimal.Parse("10.00");
+            decimal retornado;
+            target.Tarifa = esperado;
+            retornado = target.Tarifa;
+            Assert.AreEqual(esperado, retornado);
+        }
+
+        [TestMethod()]
+        public void StatusNormalContaTest()
+        {
+            Normal target = new Normal();
+            bool esperado = true;
+            bool retornado;
+            target.statusDaConta = esperado;
+            retornado = target.statusDaConta;
+            target.setBloqueado(retornado);
+            Assert.AreEqual(esperado, retornado);
+ 
+        }
+
+
+
+
+       
+    
+
+    }
+}
