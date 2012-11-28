d4862544a0b27a5fd8dc767e1982486130df019a
GLEBSON GONZAGA <glebson.tomoru@gmail.com>
2012-11-28 02:27:46 -0200

    Altualização da Classe ContaCorrente
    1) Inserção do metodo EncerrarContaCorrente
    2) Inserção do metodo BloquearContaCorrente
    3) Inserção do metodoDesbloquearContaCorrente

--- Fontes/Infnet.EngSoftSistBancario.Testes/Modelo/ContaCorrenteTest.cs
+++ Fontes/Infnet.EngSoftSistBancario.Testes/Modelo/ContaCorrenteTest.cs
@@ -37,5 +37,62 @@ namespace Infnet.EngSoftSistBancario.Testes
             ContaCorrenteEspecial contaCorrenteEspecial = rContaCorrente.CriarContaCorrente(pessoaFisica, "0312", 11, 100);
             Assert.IsNotNull(contaCorrenteEspecial);
         }
-    }
+
+     
+        [Test]
+        public void EncerrarContaCorrente() {
+
+            PessoaFisica pessoaFisica = new PessoaFisica();
+            ContaCorrente contaCorrente = new ContaCorrente ();
+            pessoaFisica.Nome = "Glebson Lima";
+            contaCorrente.Titular = pessoaFisica;
+            contaCorrente.Agencia = "031";
+            contaCorrente.Numero = "8547212";
+            contaCorrente.Tarifa = Decimal.Parse("15,00");  
+            contaCorrente.Encerrar();
+            StatusContaCorrente atual = contaCorrente.Status;
+            StatusContaCorrente esperado = StatusContaCorrente.Encerrada;
+            Assert.AreEqual(esperado, atual);
+
+        
+        }
+
+        [Test]
+        public void BloquearContaCorrente()
+        {
+
+            PessoaFisica pessoaFisica = new PessoaFisica();
+            ContaCorrente contaCorrente = new ContaCorrente();
+            pessoaFisica.Nome = "Ana Carolina";
+            contaCorrente.Titular = pessoaFisica;
+            contaCorrente.Agencia = "068";
+            contaCorrente.Numero = "8525691";
+            contaCorrente.Tarifa = Decimal.Parse("35,00");
+            contaCorrente.Bloquear();
+            StatusContaCorrente atual = contaCorrente.Status;
+            StatusContaCorrente esperado = StatusContaCorrente.Bloqueada;
+            Assert.AreEqual(esperado, atual);
+
+
+        }
+
+        [Test]
+        public void DesbloquearContaCorrente()
+        {
+
+            PessoaFisica pessoaFisica = new PessoaFisica();
+            ContaCorrente contaCorrente = new ContaCorrente();
+            pessoaFisica.Nome = "Allan Oliveira";
+            contaCorrente.Titular = pessoaFisica;
+            contaCorrente.Agencia = "052";
+            contaCorrente.Numero = "3526980";
+            contaCorrente.Tarifa = Decimal.Parse("20,00");
+            contaCorrente.Desbloquear();
+            StatusContaCorrente atual = contaCorrente.Status;
+            StatusContaCorrente esperado = StatusContaCorrente.Ativa;
+            Assert.AreEqual(esperado, atual);
+
+        }
+
+      }
 }
